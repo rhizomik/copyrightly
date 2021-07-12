f3d887afd64e7a1cf3f73fcd27e54aa588e0e5a4
Roberto García <roberto@rhizomik.net>
2021-07-12 22:41:08 +0200

    Refactor manifestation list, one component for all, creator's or own

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { AboutComponent } from './about/about.component';
 import { ManifestSingleComponent } from './manifestations/manifest/manifest-single.component';
 import { ManifestationsSearchComponent } from './manifestations/search/manifestations-search.component';
 import { ManifestationsListComponent } from './manifestations/list/manifestations-list.component';
-import { ManifestationsListAllComponent } from './manifestations/list/manifestations-list-all.component';
 import { ManifestationDetailsComponent } from './manifestations/details/manifestation-details.component';
 
 const routes: Routes = [
@@ -13,7 +12,6 @@ const routes: Routes = [
   { path: 'register', component: ManifestSingleComponent, data: { isAuthenticated: true } },
   { path: 'search', component: ManifestationsSearchComponent, data: { isAuthenticated: true } },
   { path: 'list', component: ManifestationsListComponent, data: { isAuthenticated: true } },
-  { path: 'list-all', component: ManifestationsListAllComponent, data: { isAuthenticated: true } },
   { path: 'about', component: AboutComponent , data: { isAuthenticated: false } },
   { path: '', redirectTo: '/about', pathMatch: 'full' },
 ];
--- src/app/manifestations/list/manifestations-list.component.ts
+++ src/app/manifestations/list/manifestations-list.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Subject } from 'rxjs';
-import { filter, mergeMap, takeUntil } from 'rxjs/operators';
+import { takeUntil } from 'rxjs/operators';
 import { AlertsService } from '../../alerts/alerts.service';
 import { Web3Service } from '../../util/web3.service';
 import { ManifestationsContractService } from '../manifestations-contract.service';
 import { AuthenticationService } from '../../navbar/authentication.service';
 import { ManifestEvent } from '../manifest-event';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-manifestations-list',
@@ -14,24 +15,27 @@ import { ManifestEvent } from '../manifest-event';
 })
 export class ManifestationsListComponent implements OnInit, OnDestroy {
 
+  @Input() manifester = '';
   public manifestationEvents: ManifestEvent[] = [];
-  public all = false;
+  public own = false;
   private ngUnsubscribe: Subject<void> = new Subject<void>();
 
-  constructor(private web3Service: Web3Service,
+  constructor(private route: ActivatedRoute,
+              private web3Service: Web3Service,
               private manifestationsContractService: ManifestationsContractService,
               private alertsService: AlertsService,
               private authenticationService: AuthenticationService) {}
 
   ngOnInit(): void {
-    this.authenticationService.getSelectedAccount()
-      .pipe(takeUntil(this.ngUnsubscribe))
-      .pipe(filter(account => account !== ''))
-      .pipe(mergeMap((account: string) =>
-        this.manifestationsContractService.listManifestEvents(account)))
-      .subscribe(events => {
+    this.manifestationsContractService.listManifestEvents(this.manifester)
+      .subscribe((events: ManifestEvent[]) => {
             this.manifestationEvents = events;
           }, error => this.alertsService.error(error));
+    this.authenticationService.getSelectedAccount()
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(account => {
+        this.own = this.manifester === account;
+      }, error => this.alertsService.error(error));
   }
 
   ngOnDestroy() {
