f10c160e5f03c06430c57efe33997bc7f1a56cd4
Roberto García <roberto@rhizomik.net>
2021-07-13 12:13:03 +0200

    Show alert and back button if wrong creator id

--- src/app/creators/details/creator-details.component.ts
+++ src/app/creators/details/creator-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { AlertsService } from '../../alerts/alerts.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-creator-details',
@@ -10,10 +12,24 @@ export class CreatorDetailsComponent implements OnInit, OnDestroy {
 
   creatorAccount = '';
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute,
+              private location: Location,
+              private alertsService: AlertsService) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe((params: ParamMap) => this.creatorAccount = params.get('id') || '');
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      const creatorId = params.get('id');
+      if (creatorId && creatorId.match('0x[a-fA-F0-9]{40}')) {
+        this.creatorAccount = creatorId;
+      } else {
+        this.alertsService.error('An Ethereum account is expected as the creator identifier.' +
+          '\n It starts with "0x" followed by 40 hexadecimal symbols (0-9 or A-F)', 0);
+      }
+    });
+  }
+
+  back() {
+    this.location.back();
   }
 
   ngOnDestroy(): void {}
