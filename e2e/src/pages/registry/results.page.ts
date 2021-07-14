1b4fb60ab558c8e1091257ae7432edaa5192aa3c
Roberto García <roberto@rhizomik.net>
2021-07-14 19:58:57 +0200

    Use div#list to pack results and ease e2e tests

--- e2e/src/pages/registry/results.page.ts
+++ e2e/src/pages/registry/results.page.ts
@@ -2,29 +2,29 @@ import { element, by, browser, ElementArrayFinder, ExpectedConditions, ElementFi
 
 export class ResultsPage {
 
-  private mainContainer: ElementFinder;
+  private resultsList: ElementFinder;
   private results: ElementArrayFinder;
 
   constructor() {
-    this.mainContainer = element(by.css('main.container'));
+    this.resultsList = element(by.css('div#list'));
     this.results = element.all(by.css('.card'));
   }
 
   async getResultTitleValue(num: number): Promise<string> {
-    browser.wait(ExpectedConditions.presenceOf(this.mainContainer));
+    browser.wait(ExpectedConditions.presenceOf(this.resultsList));
     return this.results.get(num - 1)
       .element(by.css('.card-title')).getText();
   }
 
   async getResultAttributeValue(num: number, attribute: string): Promise<string> {
-    browser.wait(ExpectedConditions.presenceOf(this.mainContainer));
+    browser.wait(ExpectedConditions.presenceOf(this.resultsList));
     return this.results.get(num - 1)
       .element(by.cssContainingText('.card-subtitle', attribute))
       .element(by.xpath('following-sibling::*')).getText();
   }
 
   async getResultsCount(): Promise<number> {
-    browser.wait(ExpectedConditions.presenceOf(this.mainContainer));
+    browser.wait(ExpectedConditions.presenceOf(this.resultsList));
     return this.results.count();
   }
 }
