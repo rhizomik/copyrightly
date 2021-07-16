e446ecfe0f55f20bc70035367904b7fcb72392fb
Roberto García <roberto@rhizomik.net>
2021-07-16 20:27:45 +0200

    Add UploadEvidence and Manifestation's transaction to subgraph

--- graphprotocol/src/mapping.ts
+++ graphprotocol/src/mapping.ts
@@ -1,25 +1,36 @@
 import { Manifestations, ManifestEvent, AddedEvidence } from '../generated/Manifestations/Manifestations';
-import { Manifestation } from '../generated/schema';
-import { Bytes } from '@graphprotocol/graph-ts';
+import { UploadEvidenceEvent } from '../generated/UploadEvidence/UploadEvidence';
+import { Manifestation, UploadEvidence } from '../generated/schema';
+import { Bytes, Value } from '@graphprotocol/graph-ts';
 
 export function handleManifestEvent(event: ManifestEvent): void {
-  let entity = new Manifestation(event.transaction.from.toHex());
+  let manifestation = new Manifestation(event.params.hash);
   let contract = Manifestations.bind(event.address);
   let authors = new Array<Bytes>();
-  entity.id = event.params.hash;
-  entity.title = event.params.title;
-  entity.evidenceCount = 0;
+  manifestation.id = event.params.hash;
+  manifestation.title = event.params.title;
+  manifestation.evidenceCount = 0;
   authors.push(event.params.manifester);
-  entity.authors = authors;
-  entity.creationTime = event.block.timestamp;
-  entity.expiryTime = entity.creationTime.plus(contract.timeToExpiry());
-  entity.save();
+  manifestation.authors = authors;
+  manifestation.creationTime = event.block.timestamp;
+  manifestation.expiryTime = manifestation.creationTime.plus(contract.timeToExpiry());
+  manifestation.set('transaction', Value.fromBytes(event.transaction.hash));
+  manifestation.save();
 }
 
-export function handleAddedEvidence(event: AddedEvidence): void {
-  let entity = Manifestation.load(event.transaction.from.toHex());
-  if (entity) {
-    entity.evidenceCount = entity.evidenceCount + 1;
-    entity.save();
+export function handleAddedEvidence(event: AddedEvidence): void {}
+
+export function handleUploadEvidenceEvent(event: UploadEvidenceEvent): void {
+  let uploadEvidence = new UploadEvidence(event.params.evidenceHash);
+  uploadEvidence.id = event.params.evidenceHash;
+  uploadEvidence.registry = event.params.registry;
+  uploadEvidence.evidenced = event.params.evidencedHash;
+  uploadEvidence.evidencer = event.params.evidencer;
+  uploadEvidence.set('transaction', Value.fromBytes(event.transaction.hash));
+  uploadEvidence.save();
+  let manifestation = Manifestation.load(event.params.evidencedHash);
+  if (manifestation) {
+    manifestation.evidenceCount = manifestation.evidenceCount + 1;
+    manifestation.save();
   }
 }
