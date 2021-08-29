7af2fbacc93d5a425de22cdeb4d9c94cde78a4ff
Roberto García <roberto@rhizomik.net>
2021-08-29 12:54:17 +0200

    Update subgraph.yaml and redeploy to The Graph to monitor new contracts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  thegraph: 'https://api.studio.thegraph.com/query/1303/copyrightly/0.0.4',
+  thegraph: 'https://api.studio.thegraph.com/query/1303/copyrightly/0.0.5',
   infuraToken: '${INFURA_TOKEN}'
 };
