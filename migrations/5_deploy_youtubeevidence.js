4945e6d84bc724ca5bbec9dea4531c7fd848d431
Roberto García <roberto@rhizomik.net>
2021-09-21 12:02:17 +0200

    Add Mock for YouTubeEvidence contract and deploy it when on development network

--- migrations/5_deploy_youtubeevidence.js
+++ migrations/5_deploy_youtubeevidence.js
@@ -1,12 +1,20 @@
 const Manifestations = artifacts.require("./Manifestations.sol");
 const YouTubeEvidence = artifacts.require("./YouTubeEvidence.sol");
+const YouTubeEvidenceMock = artifacts.require("./YouTubeEvidenceMock.sol");
 
 module.exports = async function(deployer, network, accounts) {
   const owner = accounts[0];
+  let youTubeEvidence;
 
-  await deployer.deploy(YouTubeEvidence, {from: owner});
-  const youTubeEvidence = await YouTubeEvidence.deployed();
-  const manifestations = await Manifestations.deployed();
-
-  await manifestations.addEvidenceProvider(youTubeEvidence.address, {from: owner});
+  deployer.then(async () => {
+    if (network === "development") {
+      await deployer.deploy(YouTubeEvidenceMock, {from: owner});
+      youTubeEvidence = await YouTubeEvidenceMock.deployed();
+    } else {
+      await deployer.deploy(YouTubeEvidence, {from: owner});
+      youTubeEvidence = await YouTubeEvidence.deployed();
+    }
+    const manifestations = await Manifestations.deployed();
+    await manifestations.addEvidenceProvider(youTubeEvidence.address, {from: owner});
+  });
 };
